8c5d7c09c087ef435d0a170317e5b2114cdeaeb3
CoffeeIsLike <sfireal2030@gmail.com>
2022-07-05 08:47:09 +0900

    updated

--- FirstProject/src/main/java/com/coffee/practice/HomeController.java
+++ FirstProject/src/main/java/com/coffee/practice/HomeController.java
@@ -1,39 +1,29 @@
 package com.coffee.practice;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import javax.servlet.http.HttpServletRequest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-/**
- * Handles requests for the application home page.
- */
+
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+	// /home.do 요청에 대해 처리할 메소드 .do 는 생략 가능하다
+	@RequestMapping("/home.do") 
+	public String home(HttpServletRequest request) {
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+		// /WEB-INF/views/home.jsp 페이지로 forward 이동해서 응답
 		return "home";
 	}
+	
+	// aspect 테스트용 메소드 
+	@RequestMapping("/aspect/home")
+	public ModelAndView authHome(HttpServletRequest request, ModelAndView mView) {
+		
+		mView.setViewName("home");
+		return mView;
+	}	
 	
 }
